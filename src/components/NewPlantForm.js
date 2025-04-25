67b2ce2cefc9d4509840f85a8159456bd284a908
Owen <owenkaris9512@gmail.com>
2025-04-25 11:53:59 +0300

    one test failing

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm( {onAddNewPlant} ) {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+    price: ""
+  });
+
+  function handleInput(event){
+    setFormData(prev => ({
+      ...prev,
+      [event.target.name]: event.target.value
+    }));
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+    .then((r) => r.json())
+    .then((newPlant) => {
+      onAddNewPlant(newPlant);
+      console.log('New Plant Added:', newPlant)
+      setFormData({
+        name: "",
+        image: "",
+        price: ""
+      });      
+    })
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Plant name" value={formData.name} onChange={handleInput}/>
+        <input type="text" name="image" placeholder="Image URL" value={formData.image} onChange={handleInput} />
+        <input type="number" name="price" step="0.01" placeholder="Price" value={formData.price} onChange={handleInput}/>
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant, onDelete }) {
+  
+  const [inStock, setInStock] = useState(true);
+
+  function handleToggleButton(){
+    setInStock((inStock) => !inStock)
+  }
+
+  function handleDelete(id){
+    fetch(`http://localhost:6001/plants/${id}`, {
+      method: "DELETE",
+    })
+    .then(r => {
+    if (r.ok) {
+        onDelete(plant)
+        console.log('Plant Deleted:', plant)
+    } else {
+      console.error('Failed to delete plant')
+    }
+    })
+    .catch((error) => console.error(error))
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      {inStock ? (
+        <button className="primary" onClick={handleToggleButton}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleToggleButton}>Out of Stock</button>
       )}
+      <button style={{color: "red"}} onClick={() => handleDelete(plant.id)}>Delete</button>
     </li>
   );
 }

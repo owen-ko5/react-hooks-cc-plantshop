9438b5f942e9be741e7c9d47c31b6e058ae3560b
Owen <owenkaris9512@gmail.com>
2025-04-25 12:22:14 +0300

    wtf

--- src/__tests__/test_suites/InStock.test.js
+++ src/__tests__/test_suites/InStock.test.js
@@ -9,18 +9,14 @@ describe('3rd Deliverable', () => {
 
     const { findAllByTestId, findByText } = render(<App />);
 
-    // Get all plant items
     const plantItems = await findAllByTestId('plant-item');
     expect(plantItems).toHaveLength(basePlants.length);
 
-    // Select the first plant item
     const firstPlantItem = plantItems[0];
 
-    // Find and click the "In Stock" button within the first plant item
     const inStockButton = within(firstPlantItem).getByText('In Stock');
     fireEvent.click(inStockButton);
 
-    // Wait for the "Out of Stock" button to appear and verify its presence
     const outOfStockButton = await findByText('Out of Stock');
     expect(outOfStockButton).toBeInTheDocument();
   });
